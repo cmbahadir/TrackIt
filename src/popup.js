1c3eff422d5b12ef7ce4ae206d0712e64ee61ef8
cmbahadir <c.mete.bahadir@gmail.com>
2022-10-02 16:53:50 +0300

    Initial commit.

--- src/popup.js
+++ src/popup.js
@@ -2,7 +2,7 @@
 
 import './popup.css';
 
-(function() {
+(function () {
   // We will make use of Storage API to get and store `count` value
   // More information on Storage API can we found at
   // https://developer.chrome.com/extensions/storage
@@ -109,4 +109,35 @@ import './popup.css';
       console.log(response.message);
     }
   );
+
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    const url = new URL(tabs[0].url);
+    let icao = "ICAO";
+    let flight = "flight";
+    if (url.origin.indexOf("flightradar24") > -1) {
+      let paths = url.pathname.split('/');
+      icao = paths[paths.length - 1];
+      flight = paths[paths.length - 2];
+      document.getElementById('RadarBox').innerHTML = "<a href='https://radarbox.com/flight/" + flight + "'>YES</a>";
+      document.getElementById('FlightAware').innerHTML = "<a href='https://flightaware.com/live/flight/" + flight + "'>YES</a>";
+      document.getElementById('ADSBExchange').innerHTML = "<a href='https://globe.adsbexchange.com/?icao=" + icao + "'>YES</a>";
+      document.getElementById('Flightradar24').innerHTML = "<a href='https://flightradar24.com/" + flight + "/" + icao + "'>YES</a>";
+    }
+    if (url.origin.indexOf("radarbox") > -1) {
+      let paths = url.pathname.split('/');
+      flight = paths[paths.length - 1];
+    }
+    if (url.origin.indexOf("adsbexchange") > -1) {
+      let paths = url.pathname.split('/').split('=');
+      icao = paths[paths.length - 1];
+    }
+    if (url.origin.indexOf("flightaware") > -1) {
+      let paths = url.pathname.split('/');
+      flight = paths[paths.length - 1];
+    }
+    // use `url` here inside the callback because it's asynchronous!
+    console.log("CURRENT URL:" + url);
+    document.getElementById('ICAO').textContent = icao;
+    
+  })
 })();
