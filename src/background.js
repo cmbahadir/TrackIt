e59d5e227e106179a8e038817c8f8ff49b92bda3
cmbahadir <c.mete.bahadir@gmail.com>
2023-03-18 13:55:20 +0300

    Fix for #1.

--- src/background.js
+++ src/background.js
@@ -1,2 +1,12 @@
 // 'use strict';
-//TODO: May be used for some background tasks.
+// background.js
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (activeInfo.tabId === !changeInfo.url.startsWith("https://www.flightradar24.com/")) {
+            chrome.action.disable(tabId);
+        } else {
+            chrome.action.enable(tabId);
+        }
+    })
+})
