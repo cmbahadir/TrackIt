c0efcbc4fc842879da26327430009f7369eaf05c
cmbahadir <c.mete.bahadir@gmail.com>
2023-03-26 09:39:41 +0300

    Workaround for disabling the extension.

--- src/background.js
+++ src/background.js
@@ -1,23 +1,32 @@
 // background.js
 
-chrome.runtime.onInstalled.addListener(() => {
-    // disable the action by default
-    chrome.action.disable();
-  
-    // remove existing rules so only ours are applied
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-      // add a custom rule
-      chrome.declarativeContent.onPageChanged.addRules([
-        {
-          // define the rule's conditions
-          conditions: [
-            new chrome.declarativeContent.PageStateMatcher({
-              pageUrl: { hostSuffix: "flightradar24.com" },
-            }),
-          ],
-          // show the action when conditions are met
-          actions: [new chrome.declarativeContent.ShowAction()],
+chrome.declarativeContent.onPageChanged.removeRules(async () => {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostPrefix: '*flightradar24.*' },
+      }),
+    ],
+    actions: [
+      new chrome.declarativeContent.SetIcon({
+        imageData: {
+          16: await loadImageData('icons/icon_16.png'),
+          32: await loadImageData('icons/icon_32.png'),
         },
-      ]);
-    });
-  });
+      }),
+      chrome.declarativeContent.ShowAction
+        ? new chrome.declarativeContent.ShowAction()
+        : new chrome.declarativeContent.ShowPageAction(),
+    ],
+  }]);
+});
+
+// SVG icons aren't supported yet
+async function loadImageData(url) {
+  const img = await createImageBitmap(await (await fetch(url)).blob());
+  const {width: w, height: h} = img;
+  const canvas = new OffscreenCanvas(w, h);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0, w, h);
+  return ctx.getImageData(0, 0, w, h);
+}
